12abc9a4e8e00d059f721cec6849fdb615d0d46b
pnck <hio131@gmail.com>
2021-02-07 02:08:07 +0800

    [WIP] working with get_info()/source_info_reader

--- foo_input_ncm/input_ncm.cpp
+++ foo_input_ncm/input_ncm.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "input_ncm.h"
 #include "cipher/cipher.h"
-#include "rapidjson/include/rapidjson/document.h"
+
 #include "rapidjson/include/rapidjson/stringbuffer.h"
 #include "rapidjson/include/rapidjson/prettywriter.h"
 
@@ -54,55 +54,86 @@ void input_ncm::open(service_ptr_t<file> p_filehint, const char * p_path, t_inpu
     if (magic != parsed_file_.magic) {
         throw_format_error();
     }
+    // skip gap
     file_ptr_->source_->seek_ex(2, file::t_seek_mode::seek_from_current, p_abort);
-    switch (p_reason) {
-    case t_input_open_reason::input_open_info_read: {
-        file_ptr_->source_->read(&parsed_file_.rc4_seed_len, sizeof(parsed_file_.rc4_seed_len), p_abort);
-        if (0 == parsed_file_.rc4_seed_len || parsed_file_.rc4_seed_len > 256) {
-            throw_format_error();
-        }
-        file_ptr_->source_->seek_ex(parsed_file_.rc4_seed_len, file::t_seek_mode::seek_from_current, p_abort);
-        file_ptr_->source_->read(&parsed_file_.meta_len, sizeof(parsed_file_.meta_len), p_abort);
-    } break;
-    case t_input_open_reason::input_open_decode: {
-        file_ptr_->source_->read(&parsed_file_.rc4_seed_len, sizeof(parsed_file_.rc4_seed_len), p_abort);
-        if (0 == parsed_file_.rc4_seed_len || parsed_file_.rc4_seed_len > 256) {
-            throw_format_error();
-        }
-        auto data_size = cipher::aligned(parsed_file_.rc4_seed_len);
-        auto data = std::make_unique<uint8_t[]>(data_size);
-        file_ptr_->source_->read(data.get(), parsed_file_.rc4_seed_len, p_abort);
-        std::for_each_n(data.get(), parsed_file_.rc4_seed_len, [](uint8_t& _b) {_b ^= 0x64; });
-        cipher::make_AES_context_with_key(ncm_rc4_seed_aes_key)
-            .set_chain_mode(cipher::aes_chain_mode::ECB)
-            .set_input(data.get(), parsed_file_.rc4_seed_len)
-            .set_output(data.get(), data_size)
-            .decrypt_all();
-        if (strncmp(reinterpret_cast<char*>(data.get()), "neteasecloudmusic", 17)) {
-            throw_format_error();
-        }
 
-        file_ptr_->rc4_decrypter = cipher::abnormal_RC4({
-                data.get() + 17,
-                data.get() + parsed_file_.rc4_seed_len - cipher::guess_padding(data.get() + parsed_file_.rc4_seed_len)
-            });
-        file_ptr_->source_->read(&parsed_file_.meta_len, sizeof(parsed_file_.meta_len), p_abort);
-        if (0 == parsed_file_.meta_len) {
-            FB2K_console_formatter() << "[WARN] No meta data found in ncm file: " << file_path_;
-        }
-        file_ptr_->source_->seek_ex(parsed_file_.meta_len, file::t_seek_mode::seek_from_current, p_abort);
-        file_ptr_->source_->seek_ex(sizeof(parsed_file_.unknown_data), file::t_seek_mode::seek_from_current, p_abort);
-        file_ptr_->source_->read(&parsed_file_.album_image_size, sizeof(parsed_file_.album_image_size), p_abort);
-        file_ptr_->source_->seek_ex(parsed_file_.album_image_size[0], file::t_seek_mode::seek_from_current, p_abort);
-        // remember where audio content starts
-        file_ptr_->audio_content_off_ = file_ptr_->source_->get_position(p_abort);
-    }break;
-    default:
-        uBugCheck();
+    // extract rc4 key for audio content decoding
+    file_ptr_->source_->read(&parsed_file_.rc4_seed_len, sizeof(parsed_file_.rc4_seed_len), p_abort);
+    if (0 == parsed_file_.rc4_seed_len || parsed_file_.rc4_seed_len > 256) {
+        throw_format_error();
     }
+    auto data_size = cipher::aligned(parsed_file_.rc4_seed_len);
+    auto data = std::make_unique<uint8_t[]>(data_size);
+    file_ptr_->source_->read(data.get(), parsed_file_.rc4_seed_len, p_abort);
+    std::for_each_n(data.get(), parsed_file_.rc4_seed_len, [](uint8_t& _b) {_b ^= 0x64; });
+    cipher::make_AES_context_with_key(ncm_rc4_seed_aes_key)
+        .set_chain_mode(cipher::aes_chain_mode::ECB)
+        .set_input(data.get(), parsed_file_.rc4_seed_len)
+        .set_output(data.get(), data_size)
+        .decrypt_all();
+    if (strncmp(reinterpret_cast<char*>(data.get()), "neteasecloudmusic", 17)) {
+        throw_format_error();
+    }
+    file_ptr_->rc4_decrypter = cipher::abnormal_RC4({
+            data.get() + 17,
+            data.get() + parsed_file_.rc4_seed_len - cipher::guess_padding(data.get() + parsed_file_.rc4_seed_len)
+        });
+
+    // get meta info json
+    file_ptr_->source_->read(&parsed_file_.meta_len, sizeof(parsed_file_.meta_len), p_abort);
+    if (0 == parsed_file_.meta_len) {
+        FB2K_console_formatter() << "[WARN] No meta data found in ncm file: " << file_path_;
+        goto NOMETA;
+    }
+    [this, &p_abort] {
+        auto meta_b64 = std::make_unique<char[]>(parsed_file_.meta_len + 1);
+        file_ptr_->source_->read(meta_b64.get(), parsed_file_.meta_len, p_abort);
+        std::for_each_n(meta_b64.get(), parsed_file_.meta_len, [](auto &b) {b ^= 0x63; });
+        meta_b64[parsed_file_.meta_len] = '\0';
+        if (strncmp(meta_b64.get(), "163 key(Don't modify):", 22)) {
+            throw_format_error();
+        }
+        auto meta_decrypt_buffer_size = pfc::base64_decode_estimate(meta_b64.get() + 22);
+        auto meta_raw = std::make_unique<uint8_t[]>(meta_decrypt_buffer_size);
+        pfc::base64_decode(meta_b64.get() + 22, meta_raw.get());
+        auto total = cipher::make_AES_context_with_key(ncm_meta_aes_key)
+            .set_chain_mode(cipher::aes_chain_mode::ECB)
+            .set_input(meta_raw.get(), meta_decrypt_buffer_size)
+            .set_output(meta_raw.get(), meta_decrypt_buffer_size)
+            .decrypt_all().outputted_len();
+        total -= cipher::guess_padding(meta_raw.get() + meta_decrypt_buffer_size);
+        meta_raw[total] = '\0';
+        if (strncmp(reinterpret_cast<char*>(meta_raw.get()), "music:", 6)) {
+            throw_format_error();
+        }
+        // skip heading `music:`
+        meta_str_.assign(reinterpret_cast<char*>(meta_raw.get()) + 6);
+        meta_json_.Parse(meta_str_.c_str());
+        if (!meta_json_.IsObject()) {
+            FB2K_console_formatter() << "[WARN] Failed to parse meta info of ncm file: " << file_path_;
+        }
+    }();
+NOMETA:
+    // skip gap
+    file_ptr_->source_->seek_ex(sizeof(parsed_file_.unknown_data), file::t_seek_mode::seek_from_current, p_abort);
+    // get album image
+    file_ptr_->source_->read(&parsed_file_.album_image_size, sizeof(parsed_file_.album_image_size), p_abort);
+    if (!parsed_file_.album_image_size[0]) {
+        FB2K_console_formatter() << "[WARN] No album image found in ncm file: " << file_path_;
+        goto NOALBUMIMG;
+    }
+    [this, &p_abort] {
+        image_data_.resize(parsed_file_.album_image_size[0]);
+        file_ptr_->source_->read(image_data_.data(), image_data_.size(), p_abort);
+    }();
+NOALBUMIMG:
+    // remember where audio content starts
+    file_ptr_->audio_content_offset_ = file_ptr_->source_->get_position(p_abort);
+
 }
 
 void input_ncm::decode_seek(double p_seconds, abort_callback & p_abort) {
+    // seek position is masqueraded by ncm_file
     decoder_->seek(p_seconds, p_abort);
 }
 
@@ -111,30 +142,104 @@ bool input_ncm::decode_run(audio_chunk & p_chunk, abort_callback & p_abort) {
 }
 
 void input_ncm::decode_initialize(unsigned p_flags, abort_callback & p_abort) {
-    if (!file_ptr_->audio_content_off_) {
+    if (!file_ptr_->audio_content_offset_) {
+        // initialize should always follow open
         uBugCheck();
     }
 
-    //file_ptr_->source_->seek(file_ptr_->audio_content_off_, p_abort);
-    service_list_t<input_entry> result;
-    input_entry::g_find_inputs_by_content_type(result, "audio/mpeg", false);
-    service_ptr_t<input_entry_v2> ptr;
-    result[0]->cast(ptr);
+    service_list_t<input_entry> input_services;
+    do {
+        // there is format hint, so we don't have to find_input twice or more
+        if (meta_json_.IsObject() && meta_json_.HasMember("format")) {
+            if (meta_json_["format"] == "flac") {
+                input_entry::g_find_inputs_by_content_type(input_services, "audio/flac", false);
+                break;
+            }
+            else if (meta_json_["format"] == "mp3") {
+                input_entry::g_find_inputs_by_content_type(input_services, "audio/mpeg", false);
+                break;
+            }
+        }
+        // unable to determine decoder directly,  guess possible
+        service_list_t<input_entry> mpeg_decoders, flac_decoders;
+        input_entry::g_find_inputs_by_content_type(mpeg_decoders, "audio/mpeg", false);
+        input_entry::g_find_inputs_by_content_type(flac_decoders, "audio/flac", false);
+        input_services.add_items(mpeg_decoders);
+        input_services.add_items(flac_decoders);
+    } while (false);
 
-    FB2K_console_formatter() << FB2K_DebugLog() << "Actual decoder for ncm file: " << ptr->get_name();
-    ptr->open_for_decoding(decoder_, file_ptr_, file_path_, p_abort);
-    if (decoder_.is_empty()) {
-        throw exception_service_not_found("Cannot get audio decoder.");
+    service_ptr_t<input_entry_v3> input_ptr;
+
+    for (size_t i = 0; i < input_services.get_count(); ++i) {
+        input_services[i]->cast(input_ptr);
+        try {
+            if (input_ptr.is_valid()) {
+                input_ptr->open_for_decoding(decoder_, file_ptr_, file_path_, p_abort);
+                if (decoder_.is_valid()) {
+                    decoder_->initialize(0, p_flags, p_abort);
+                    FB2K_console_formatter() << "Actual decoder for ncm file: " << input_ptr->get_name();
+                    break;
+                }
+            }
+        }
+        catch (const pfc::exception& /*e*/) {
+            // pass
+        }
     }
-    decoder_->initialize(0, p_flags, p_abort);
+    if (decoder_.is_empty()) {
+        // final attempt: enumerate all input_entry and test if who accepting the audio content
+        service_enum_t<input_entry> input_enum;
+        while (input_enum.next(input_ptr)) {
+            try {
+                if (input_ptr.is_valid()) {
+                    input_ptr->open_for_decoding(decoder_, file_ptr_, file_path_, p_abort);
+                    if (decoder_.is_valid()) {
+                        decoder_->initialize(0, p_flags, p_abort);
+                        FB2K_console_formatter() << "Actual decoder for ncm file: " << input_ptr->get_name();
+                        break;
+                    }
+                }
+            }
+            catch (const pfc::exception& /*e*/) {
+                // pass
+            }
+        }
+    }
+
+    if (decoder_.is_empty()) {
+        throw exception_service_not_found("Failed to find proper audio decoder.");
+    }
+    input_ptr->open_for_info_read(source_info_reader_, file_ptr_, file_path_, p_abort);
 }
 
 t_filestats input_ncm::get_file_stats(abort_callback & p_abort) {
+    // return real file stats, which would be displayed on Properties/Location
     return file_ptr_->source_->get_stats(p_abort);
 }
 
 void input_ncm::get_info(file_info & p_info, abort_callback & p_abort) {
+    if (source_info_reader_.is_valid()) {
+        source_info_reader_->get_info(0, p_info, p_abort);
+    }
+    if (meta_json_.IsNull()) {
+        return;
+    }
+    p_info.set_length(meta_json_["duration"].GetInt() / 1000.0);
+    for (auto &v : meta_json_["artist"].GetArray()) {
+        p_info.meta_add("Artist", v[0].GetString());
+    }
+    p_info.meta_set("Album", meta_json_["album"].GetString());
+    p_info.meta_set("Title", meta_json_["musicName"].GetString());
+    p_info.info_set_bitrate(meta_json_["bitrate"].GetUint64() / 1000);
+    p_info.info_set("Music ID", PFC_string_formatter() << meta_json_["musicId"].GetUint64());
+    p_info.info_set("Album ID", PFC_string_formatter() << meta_json_["albumId"].GetUint64());
+    //auto album_art = album_art_data_impl::g_create(image_data_.data(), image_data_.size());
+    //static_api_ptr_t<album_art_manager_v3>()->
+}
 
+bool fb2k_ncm::input_ncm::decode_get_dynamic_info(file_info & p_out, double & p_timestamp_delta)
+{
+    return decoder_->get_dynamic_info(p_out, p_timestamp_delta);
 }
 
 
@@ -146,9 +251,9 @@ static input_singletrack_factory_t<input_ncm> g_input_ncm_factory;
 class debug_initquit : public initquit {
 public:
     void on_init() {
-        service_enum_t<input_entry> input_services;
-        service_ptr_t<input_entry_v3> ptr;
-        while (input_services.next(ptr)) {
+        service_enum_t<input_entry> input_enum;
+        service_ptr_t<input_entry_v2> ptr;
+        while (input_enum.next(ptr)) {
             FB2K_console_formatter() << ptr->get_name() << " " << pfc::print_guid(ptr->class_guid);
         }
 
